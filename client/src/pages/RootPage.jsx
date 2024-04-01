9bfded60808b19f09be2cb953d95e1090a2a0905
Milosz08 <gilgamilosz451@gmail.com>
2024-04-01 23:25:54 +0200

    add flowbite react library and append test alert

--- client/src/pages/RootPage.jsx
+++ client/src/pages/RootPage.jsx
@@ -1,9 +1,27 @@
 'use strict';
+
 /*
  * Part of Silesian University of Technology project.
  * Created only for learning purposes.
  */
+import { useState } from 'react';
+import { Alert } from 'flowbite-react';
 
-const RootPage = () => <div>ROOT</div>;
+const RootPage = () => {
+  const [modalVisibility, setmodalVisibility] = useState(true);
+
+  return (
+    <div>
+      {modalVisibility && (
+        <Alert
+          color="success"
+          onDismiss={() => setmodalVisibility(prevState => !prevState)}>
+          <span className="font-medium">Info alert!</span> Change a few things
+          up and try submitting again.
+        </Alert>
+      )}
+    </div>
+  );
+};
 
 export default RootPage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,20 @@
 'use strict';
+
 /*
  * Part of Silesian University of Technology project.
  * Created only for learning purposes.
  */
+import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    '../node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite],
 };
