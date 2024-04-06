ace12132c68120400fab2b21d26f9194e49b5afd
Łukasz Krawczyk <101646747+trimplexx@users.noreply.github.com>
2024-04-06 23:46:26 +0200

    feat/0002 - add navbar, meeting code input [FE] (#2)
    * feat/0002 - add navbar, meeting code input [FE]
    
    * fix review

--- client/src/router/Entrypoint.jsx
+++ client/src/router/Entrypoint.jsx
@@ -7,6 +7,7 @@
 import * as React from 'react';
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import SuspenseLoader from '../components/SuspenseLoader';
 import { AppRouter } from './AppRouter';
 
@@ -14,9 +15,12 @@ const Entrypoint = () => (
   <React.StrictMode>
     <Suspense fallback={<SuspenseLoader />}>
       <BrowserRouter>
-        {/* Define here shared content upper outlet, ex header */}
-        <AppRouter />
-        {/* Define here shared content below outlet, ex footer */}
+        <div className="flex flex-col h-screen">
+          <Navbar />
+          {/* Define here shared content upper outlet, ex header */}
+          <AppRouter />
+          {/* Define here shared content below outlet, ex footer */}
+        </div>
       </BrowserRouter>
     </Suspense>
   </React.StrictMode>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,7 +14,29 @@ export default {
     '../node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: 'shake 0.5s ease-in-out',
+        gradient: 'gradient 15s ease infinite',
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '10%, 30%, 50%, 70%, 90%': { transform: 'translateX(-2px)' },
+          '20%, 40%, 60%, 80%': { transform: 'translateX(2px)' },
+        },
+        gradient: {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      width: ['group-hover'],
+    },
   },
   plugins: [flowbite],
   darkMode: 'class',
