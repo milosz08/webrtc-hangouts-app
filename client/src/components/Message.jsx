128c67e0050ce5ddbecde20a72e0e8966a4ceb95
Łukasz Krawczyk <101646747+trimplexx@users.noreply.github.com>
2024-04-09 01:52:31 +0200

    text color fix

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -9,7 +9,8 @@ import PropTypes from 'prop-types';
 const Message = ({ message, isUser, username }) => {
   return (
     <div className={`flex ${isUser ? 'justify-end' : 'justify-start'}`}>
-      <div className={`text-white ${isUser ? 'text-right' : 'text-left'}`}>
+      <div
+        className={`text-gray-600 dark:text-white ${isUser ? 'text-right' : 'text-left'}`}>
         <p className="px-4 font-bold">{username}</p>
         <div
           className={`rounded-lg px-4 py-2 mx-2 mb-2 ${isUser ? 'bg-blue-400 text-white' : 'bg-gray-200 dark:bg-[#1A314E] dark:text-white'}`}>
