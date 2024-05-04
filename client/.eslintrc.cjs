0261a6014d18a3b96bf96ad9fe23ac7bb71950ea
milosz08 <gilgamilosz451@gmail.com>
2024-05-04 03:11:11 +0200

    remove checking exports in react components

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -30,9 +30,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
   },
 };
