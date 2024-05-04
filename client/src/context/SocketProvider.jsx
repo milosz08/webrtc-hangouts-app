830a7f64de9e573a6b487eda4c5562170b4ed4df
milosz08 <gilgamilosz451@gmail.com>
2024-05-04 03:13:55 +0200

    create appContext and SocketContext providers

---
+++ client/src/context/SocketProvider.jsx
@@ -0,0 +1,28 @@
+'use strict';
+
+/*
+ * Part of Silesian University of Technology project.
+ * Created only for learning purposes.
+ */
+import { createContext, useContext, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { io } from 'socket.io-client';
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+const SocketProvider = ({ children }) => {
+  const socket = useMemo(() => io(window.reactEnvClient.serverUrl), []);
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+SocketProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default SocketProvider;
