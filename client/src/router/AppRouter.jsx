0db2dde50d0347cdd74b9ccde54a07a465bf1c54
milosz08 <gilgamilosz451@gmail.com>
2024-05-04 03:15:08 +0200

    protect /meeting route

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -5,16 +5,24 @@
  * Created only for learning purposes.
  */
 import * as React from 'react';
-import { useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
+import { useAppContext } from '../context/AppContextProvider';
 
 const NotFoundPage = React.lazy(() => import('../pages/NotFoundPage'));
 const RootPage = React.lazy(() => import('../pages/RootPage'));
 const MeetingPage = React.lazy(() => import('../pages/MeetingPage'));
 
-export const AppRouter = () =>
-  // define all routes here (https://reactrouter.com/en/main/hooks/use-routes)
-  useRoutes([
+const AppRouter = () => {
+  const { state } = useAppContext();
+
+  return useRoutes([
     { path: '/', element: <RootPage /> },
     { path: '*', element: <NotFoundPage /> },
-    { path: '/meeting', element: <MeetingPage /> },
+    {
+      path: '/meeting/:meetingId',
+      element: state.roomKey ? <MeetingPage /> : <Navigate to="/" />,
+    },
   ]);
+};
+
+export default AppRouter;
