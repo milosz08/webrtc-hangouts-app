733f77acd44c7688ae2d48ef5756ad633e4317b3
Marta-01 <piechotamarta01@gmail.com>
2024-05-06 20:57:25 +0200

    chore: change button names

--- client/src/components/ConfirmLeaving.jsx
+++ client/src/components/ConfirmLeaving.jsx
@@ -24,7 +24,7 @@ const ConfirmLeaving = ({ handleLeave, handleStay }) => {
               'mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 rounded-lg text-sm px-5 py-2.5',
               'text-center transition ease-out duration-200 focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-purple-800'
             )}>
-            Zostań
+            Stay
           </button>
           <button
             onClick={handleLeave}
@@ -32,7 +32,7 @@ const ConfirmLeaving = ({ handleLeave, handleStay }) => {
               'bg-red-500 hover:bg-red-600 text-white rounded-lg text-sm px-5 py-2.5',
               'text-center transition ease-out duration-200 focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-purple-800'
             )}>
-            Opuść spotkanie
+            Leave the meeting
           </button>
         </div>
       </div>
