7a6783950287bbfdab1cd045e71ab16003e33cfa
Łukasz Krawczyk <101646747+trimplexx@users.noreply.github.com>
2024-05-06 12:54:55 +0200

    feat/0009 - [FE] add custom 404 page (#11)

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,9 +1,34 @@
 'use strict';
+
 /*
  * Part of Silesian University of Technology project.
  * Created only for learning purposes.
  */
+import { NavLink } from 'react-router-dom';
+import NotFoundImage from '../assets/404.png';
+import CustomButton from '../components/CustomButton';
 
-const NotFoundPage = () => <div>NOT FOUND</div>;
+const NotFoundPage = () => {
+  return (
+    <div className="bg-custom-meeting dark:bg-dark-meeting h-full flex-grow flex flex-col md:flex-row overflow-hidden">
+      <div className="space-y-8 flex flex-col md:grid  md:grid-cols-3 justify-center items-center pt-8 md:pt-0 gap-8 sm:px-8 md:mt-32 mt-0 mb-auto">
+        <div className="flex flex-col items-center justify-center text-center">
+          <p className="mt-4 lg:px-10 text-xl lg:text-2xl dark:text-white text-gray-600">
+            The page you are on does not exist. Check the URL or that your
+            meeting has not ended.
+          </p>
+          <div className="py-4 md:py-8">
+            <CustomButton>
+              <NavLink to="/" className="text-sm lg:text-base">
+                Go to Home Page
+              </NavLink>
+            </CustomButton>
+          </div>
+        </div>
+        <img src={NotFoundImage} className="col-span-2" />
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
