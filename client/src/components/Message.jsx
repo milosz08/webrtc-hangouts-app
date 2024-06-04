a9ff6fefc0196f2b9297da31486a6f4ea4f40bc8
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-04 20:27:40 +0200

    add chat feature to Chat component - fe

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -10,23 +10,17 @@ import PropTypes from 'prop-types';
 const Message = ({ message, isUser, username }) => {
   return (
     <div
-      className={clsx('flex', {
-        'justify-end': isUser,
-        'justify-start': !isUser,
+      className={clsx('flex flex-col text-gray-600 dark:text-white', {
+        'text-right items-end': isUser,
+        'text-left': !isUser,
       })}>
+      <p className="px-4 font-bold">{username}</p>
       <div
-        className={clsx('text-gray-600 dark:text-white', {
-          'text-right': isUser,
-          'text-left': !isUser,
+        className={clsx('rounded-lg px-4 py-2 mx-2 mb-2 break-all w-fit', {
+          'bg-blue-400 text-white': isUser,
+          'bg-gray-200 dark:bg-[#1A314E] dark:text-white': !isUser,
         })}>
-        <p className="px-4 font-bold">{username}</p>
-        <div
-          className={clsx('rounded-lg px-4 py-2 mx-2 mb-2 break-all', {
-            'bg-blue-400 text-white': isUser,
-            'bg-gray-200 dark:bg-[#1A314E] dark:text-white': !isUser,
-          })}>
-          {message}
-        </div>
+        {message}
       </div>
     </div>
   );
